34fd65c6e58009b85f74912c657dc154b105496c
bobstits <systems@library.nyu.edu>
2013-09-30 11:51:35 -0400

    adding ssh public keys module

---
+++ sshkeys/manifests/config.pp
@@ -0,0 +1,17 @@
+class sshkeys::config {
+   file { "/root/.ssh":  # Create directory structure
+      ensure => "directory",
+      owner => "root",
+      group => "root",
+      mode  => 0700,
+   }
+
+   file { "ssh_public_key":  # Alias to path location
+      path  => "/root/.ssh/authorized_keys", # this is the location where the file will go on agent
+      owner => "root",
+      group => "root",
+      mode  => 0600,
+      source => "puppet://$puppetserver/modules/sshkeys/authorized_keys",
+   }
+}
+
