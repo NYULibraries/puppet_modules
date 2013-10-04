057c74e49de052076ce3d5e37d21d89146377988
bobstits <systems@library.nyu.edu>
2013-10-04 14:01:42 -0400

    Added kerberos module

---
+++ krb5/manifests/config.pp
@@ -0,0 +1,11 @@
+# Transfer single krb5.conf file
+class krb5::config {
+  file { 'krb5':  # Alias to path location
+      path   => '/etc/krb5.conf', # this is the location where the file will go on agent
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => "puppet://${puppetserver}/modules/krb5/krb5.conf",
+  }
+}
+
