edbdff54369b8c93d85570903b7555a9a0cfe992
bobstits <systems@library.nyu.edu>
2013-10-07 16:57:59 -0400

    Update config.pp to reference templates

--- pam/manifests/config.pp
+++ pam/manifests/config.pp
@@ -1,3 +1,4 @@
+# Template that sets up custom pam config files
 class pam::config {
   case $operatingsystem {
       RedHat:
@@ -5,22 +6,22 @@ class pam::config {
         # RHEL 5.x
         if $operatingsystemmajrelease == '5' {
           file { 'systems-auth':  # Alias to path location rhel5.x
-              path  => '/etc/pam.d/systems-auth-ac', # this is the location where the file will go on agent
-              owner  => 'root',
-              group  => 'root',
-              mode   => '0644',
-              source => "puppet://${puppetserver}/modules/pam/systems-auth-ac",
+              path    => '/etc/pam.d/systems-auth-ac', # this is the location where the file will go on agent
+              owner   => 'root',
+              group   => 'root',
+              mode    => '0644',
+              content => template('pam/system-auth-ac.erb'),  # Use file.erb to fill content of password-auth-ac file
           }
         }
 
         # RHEL 6.x
         elsif $operatingsystemmajrelease == '6' {
           file { 'password-auth':  # Alias to path location
-              path  => '/etc/pam.d/password-auth-ac', # this is the location where the file will go on agent
-              owner  => 'root',
-              group  => 'root',
-              mode   => '0644',
-              source => "puppet://${puppetserver}/modules/pam/password-auth-ac",
+              path    => '/etc/pam.d/password-auth-ac', # this is the location where the file will go on agent
+              owner   => 'root',
+              group   => 'root',
+              mode    => '0644',
+              content => template('pam/password-auth-ac.erb'), # Use file.erb to fill content of password-auth-ac file
           }
         }
 
