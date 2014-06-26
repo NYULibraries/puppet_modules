d732398eabb1c274a18006c3a14fdad353b26f5d
bobstits <systems@library.nyu.edu>
2014-06-26 11:37:42 -0400

    added ams1 and amsdev1 custom config to config.pp file

--- sshd/manifests/config.pp
+++ sshd/manifests/config.pp
@@ -1,13 +1,26 @@
 # Transfer single sshd_config file
 class sshd::config {
-  file { 'sshd':  # Alias to path location
-      path    => '/etc/ssh/sshd_config', # this is the location where the file will go on agent
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0600',
-      source  => "puppet://${puppetserver}/modules/sshd/sshd_config",
-      require => Class['sshd::install'],
-      notify  => Class['sshd::service'],
+  if ($hostname == 'amsdev1') or ($hostname == 'ams1') {
+    file { 'sshd':  # Alias to path location
+        path    => '/etc/ssh/sshd_config', # this is the location where the file will go on agent
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0600',
+        source  => "puppet://${puppetserver}/modules/sshd/sshd_config_amsdev1",
+        require => Class['sshd::install'],
+        notify  => Class['sshd::service'],
+    }
+  }
+
+  else {
+    file { 'sshd':  # Alias to path location
+        path    => '/etc/ssh/sshd_config', # this is the location where the file will go on agent
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0600',
+        source  => "puppet://${puppetserver}/modules/sshd/sshd_config",
+        require => Class['sshd::install'],
+        notify  => Class['sshd::service'],
+    }
   }
 }
-
