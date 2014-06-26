e990511ad79f333cdab4142f9c896b0645c2204b
bobstits <systems@library.nyu.edu>
2014-06-26 11:49:10 -0400

    Updates openssl to 1.0.1e-16 to patch up heartbleed bug

---
+++ openssl/manifests/install.pp
@@ -0,0 +1,13 @@
+# Make sure openssl packages are installed
+# Fix for hearbleed openssl vulnerability.
+# Install openssl-1.0.1e-16.el6_5.7
+
+class openssl::install {
+  # Only for RHEL6.x
+  if $operatingsystemmajrelease == '6' {
+    package{ 'openssl': 
+      ensure => "1.0.1e-16.el6_5.7"
+    }
+  }
+}
+
