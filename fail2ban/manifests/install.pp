3bfecfdb7ee646ae8a58b4965ea06497cd4711a3
bobstits <systems@library.nyu.edu>
2015-09-09 11:27:04 -0400

    no comment

--- fail2ban/manifests/install.pp
+++ fail2ban/manifests/install.pp
@@ -3,5 +3,5 @@ class fail2ban::install {
   package{ 'shorewall': ensure => present }
   package{ 'python-inotify': ensure => present }
   package{ 'gamin-python': ensure => present }
-  package{ 'fail2ban': ensure => present }
+  package{ 'fail2ban': ensure => latest }
 }
